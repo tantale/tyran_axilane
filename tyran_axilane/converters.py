568a2ee167ac2b75e8d8c2036049488cf0ddbfe7
Laurent LAPORTE <tantale.solutions@gmail.com>
2018-01-16 22:16:36 +0100

    Improve converters: add log message, add trace file (.html).

---
+++ tyran_axilane/converters.py
@@ -0,0 +1,95 @@
+# coding: utf-8
+import logging
+import re
+
+LOG = logging.getLogger(__name__)
+
+
+def convert_line_sep(content):
+    # type: (str) -> str
+    return content.replace("\u2028", "\n")
+
+
+FLAGS = re.DOTALL | re.UNICODE | re.IGNORECASE
+
+_sub_running_title1 = re.compile(r"\bLE\s+TYRAN\s+D'AXILANE\s+Michel\s+Grimaud\s+", flags=FLAGS).sub
+_sub_running_title2 = re.compile(r"\bFolio\s+Junior\s+1998\s+", flags=FLAGS).sub
+
+
+def convert_running_title(content):
+    # "LE TYRAN D'AXILANE Michel Grimaud"
+    # "Folio Junior 1998"
+    content = _sub_running_title1("", content)
+    content = _sub_running_title2("", content)
+    return content
+
+
+# "Illustration"
+def del_illustration(mo):
+    result = mo.group(1) + "\n"
+    LOG.info("delete illustration: " + result.strip())
+    return result
+
+
+_sub_illustration = re.compile(r"\bIllustration\s+(\d+)\s+", flags=FLAGS).sub
+
+
+def convert_illustration(content):
+    return _sub_illustration(del_illustration, content)
+
+
+_sub_chapter = re.compile(r"(?<=\n)[IX|l\\/]+\s+Chapitre[ ]?\s+(.*?)\s*\n", flags=FLAGS).sub
+
+
+class Chapters:
+    def __init__(self):
+        self.nos = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII']
+        self.iter_nos = iter(self.nos)
+
+    def reset(self):
+        self.iter_nos = iter(self.nos)
+
+    def format_title(self, mo):
+        title = "Chapitre {no} – {title}".format(no=next(self.iter_nos), title=mo.group(1))
+        LOG.info("format title: " + title)
+        return "<h2>{title}</h2>\n".format(title=title)
+
+    def __call__(self, content):
+        return _sub_chapter(self.format_title, content)
+
+
+convert_chapters = Chapters()
+
+_sub_word_break = re.compile(r"(\w\w+)-\s+(\w\w+)", flags=FLAGS).sub
+
+
+def del_hyphen(mo):
+    word = "".join(mo.groups())
+    LOG.info("delete word break: " + word)
+    return word
+
+
+def convert_word_break(content):
+    return _sub_word_break(del_hyphen, content)
+
+
+_sub_folio = re.compile(r"(?:\b\d+ \n|(?<=\n)\d+\n|\bl\d+ \n|\b\d+l \n)", flags=FLAGS).sub
+
+
+def del_folio(mo):
+    LOG.info("delete folio: " + mo.group().strip())
+    return u""
+
+
+def convert_folio(content):
+    return _sub_folio(del_folio, content)
+
+
+CONVERTERS = [
+    ('convert_line_sep', convert_line_sep),
+    ('convert_running_title', convert_running_title),
+    ('convert_illustration', convert_illustration),
+    ('convert_chapters', convert_chapters),
+    ('convert_folio', convert_folio),
+    ('convert_word_break', convert_word_break),
+]
--- tyran_axilane/normalize.py
+++ tyran_axilane/normalize.py
@@ -2,49 +2,42 @@
 from __future__ import print_function
 
 import io
-import re
+import logging
+import os.path
+
+from tyran_axilane.converters import CONVERTERS
+
+LOG = logging.getLogger(__name__)
 
 
 def normalize(src_path, dst_path):
+    dst_dir, dst_name = os.path.split(dst_path)
+    if not os.path.isdir(dst_dir):
+        os.makedirs(dst_dir)
+
+    basename = os.path.basename(dst_name)
+    log_name = f"{basename}.log"
+    log_path = os.path.join(dst_dir, log_name)
+    logging.basicConfig(level=logging.INFO,
+                        format="[{levelname:<7}] {name}: {message}",
+                        filename=log_path,
+                        style='{')
+
+    LOG.info("Reading file '{0}'...".format(src_path))
     with io.open(src_path, mode="r", encoding='utf-8') as f:
         content = f.read()
 
-    # "LE TYRAN D'AXILANE Michel Grimaud"
-    # "Folio Junior 1998"
-
-    content = content.replace(u"LE TYRAN D'AXILANE Michel Grimaud\n", u"")
-    content = content.replace(u"Folio Junior 1998\n", u"")
-
-    # "I Chapitre "
-    nos = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII']
-    iter_nos = iter(nos)
-
-    def sub_title(mo):
-        title = u"<h2>Chapitre {no} – {title}</h2>\n".format(no=next(iter_nos), title=mo.group(2))
-        print(title.strip())
-        return title
-
-    content = re.sub(r"^(.*)Chapitre[ ]?[\u2028\n](.*?)[ ]?[\u2028\n]", sub_title, content, flags=re.MULTILINE | re.UNICODE)
-
-    # don- naient
-    def sub_hyphen(mo):
-        word = "".join(mo.groups())
-        print(word)
-        return word
-
-    content = re.sub(r"(\w\w+)-[ \n\u2028](\w\w+)", sub_hyphen, content, flags=re.UNICODE)
-
-    def sub_folio(mo):
-        print(mo.group().strip())
-        return u""
-
-    content = re.sub(r"\u2028?\d\d+[ ]\n", sub_folio, content, flags=re.DOTALL | re.UNICODE)
-
-    content = content.replace(u"\u2028", u"<br/>\n")
+    for counter, (converter_name, converter) in enumerate(CONVERTERS, 1):
+        name = f"{counter:02d}_{converter_name}.html"
+        path = os.path.join(dst_dir, name)
+        LOG.info(f"Running {converter_name}...")
+        content = converter(content)
+        with io.open(path, mode="w", encoding="utf-8") as f:
+            f.write(content)
 
     with io.open(dst_path, mode="w", encoding="utf-8") as f:
         f.write(content)
 
 
 if __name__ == '__main__':
-    normalize("tyran_axilane.txt", "tyran_axilane.html")
+    normalize("tyran_axilane.txt", "../target/tyran_axilane.html")
